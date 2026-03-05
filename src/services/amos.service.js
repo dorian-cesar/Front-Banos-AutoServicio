1c229722e80df91fec2354b6cbc5431c24f707de
Diegowigo <dwigodski@gmail.com>
2026-03-05 16:39:29 -0300

    test: se corrige mock checkPosStatus

--- src/services/amos.service.js
+++ src/services/amos.service.js
@@ -4,18 +4,21 @@ export async function checkPosStatus() {
   const ip = getIpFromStorage();
   if (!ip) throw new Error("No hay IP en localStorage");
 
-  const res = await fetch(`https://${ip}:3000/monitor`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  });
+  //   const res = await fetch(`https://${ip}:3000/monitor`, {
+  //     method: "GET",
+  //     headers: { "Content-Type": "application/json" },
+  //   });
 
-  if (!res.ok)
-    throw new Error("Error al obtener estado del POS: " + res.status);
+  //   if (!res.ok)
+  //     throw new Error("Error al obtener estado del POS: " + res.status);
 
-  const data = await res.json();
+  //   const data = await res.json();
+  const data = {
+    success: true,
+    server: true,
+  };
 
-  // return data.success && data.server; // true si está online, false si no
-  return true && true;
+  return data.success && data.server; // true si está online, false si no
 }
 
 export async function postPayment(payload) {
