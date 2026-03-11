3ae6f9c65e78e5d6d0471fa718ca79248cb7bbd6
Diegowigo <dwigodski@gmail.com>
2026-03-11 14:04:40 -0300

    add: se agrega modal de notificación a usuario para retirar kit de limpieza en caja en el servicio de ducha

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,34 +1,24 @@
-
 import Image from "next/image";
 
 export default function Footer() {
-    return (
-        <div
-            className="fixed bottom-0 left-0 flex items-center justify-between w-full"
-            style={{
-                padding: "0 40px",
-                minHeight: "150px",
-                backgroundColor: "#013ba7"
-            }}
-        >
+  return (
+    <div
+      className="fixed bottom-0 left-0 flex items-center justify-between w-full"
+      style={{
+        padding: "0 40px",
+        minHeight: "150px",
+        backgroundColor: "#013ba7",
+      }}
+    >
+      <div className="text-white font-bold text-4xl">Baños Terminal Sur</div>
 
-            <Image
-                src="/LOGOTIPO_PB.png"
-                alt="Logo PB"
-                width={400}
-                height={200}
-                className="invert brightness-0"
-            />
-
-            <Image
-                src="/LOGOTIPO_WIT.png"
-                alt="Logo WIT"
-                width={70}
-                height={70}
-                className="invert brightness-0 mr-3"
-            />
-
-
-        </div>
-    )
+      <Image
+        src="/LOGOTIPO_WIT.png"
+        alt="Logo WIT"
+        width={70}
+        height={70}
+        className="invert brightness-0 mr-3"
+      />
+    </div>
+  );
 }
